55e764b1c2d46997b2a1ab1624d5009120ac2c8c
sidneyramos <sidney.ramos95@gmail.com>
2020-02-18 21:50:09 +1100

    Bugfixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "fonts",
-        path: `${__dirname}/src/fonts/`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
